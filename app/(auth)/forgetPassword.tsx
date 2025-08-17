f87f3dc261e72ca83bcf3ad7d5e43f398751e404
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-08-17 15:14:34 +0100

    added a back button

--- app/(auth)/forgetPassword.tsx
+++ app/(auth)/forgetPassword.tsx
@@ -1,8 +1,11 @@
 import forget from '@/assets/icons/icon_forget.png'
 import CustomButton from '@/components/CustomButton'
 import FormField from '@/components/FormField'
+import { router } from 'expo-router'
+import { ArrowLeft } from 'lucide-react-native'
 import React, { useState } from 'react'
-import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native'
+import { Image, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native'
+
 
 const ForgetPassword = () => {
   const [form, setForm] = useState({email: ''})
@@ -15,6 +18,11 @@ const ForgetPassword = () => {
   return (
     <SafeAreaView className='bg-primary h-full'>
         <ScrollView>
+           <View className="absolute top-12 left-4 z-10">
+          <Pressable onPress={() => router.back()}>
+            <ArrowLeft size={28} color="#000" />
+          </Pressable>
+        </View>
             <View className='w-full items-center justify-center h-full px-4 my-12'>
                <View className='bg-icon_bg w-[200px] h-[200px] items-center justify-center rounded-full'>
                 <Image source={forget} resizeMode='contain' className='w-[80px] h-[80px]'/>
